dda7f6bd9faa56ecf6ae131a254885f3ab8694bf
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2025-05-27 16:16:11 +0200

    selex-area

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,11 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Main from "./components/Main";
 
-import movies from "./assets/db/Movies.js";
-
 export default function App() {
   return (
     <>
       <Header />
-      <Main movies={movies} />
+      <Main />
       <Footer />
     </>
   );
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,31 +1,58 @@
-export default function Main({ movies }) {
+import { useState } from "react";
+
+const movies = [
+  { title: "Inception", genre: "Fantascienza" },
+  { title: "Il Padrino", genre: "Thriller" },
+  { title: "Titanic", genre: "Romantico" },
+  { title: "Batman", genre: "Azione" },
+  { title: "Interstellar", genre: "Fantascienza" },
+  { title: "Pulp Fiction", genre: "Thriller" },
+];
+
+useState;
+
+// export
+export default function Main() {
   return (
     <>
       <div className="container">
         <div className="row g-2 my-2 mt-5">
-          <div className="col-4">
-            <div className="card">
-              <div className="card-head text-center p-2">lista di film</div>
-              <div className="card-body">imput per filtraggio</div>
-            </div>
-          </div>
-          <div className="col-4">
-            <div className="card">
-              <div className="card-head text-center p-2">lista di film</div>
-              <div className="card-body">imput per filtraggio</div>
-            </div>
-          </div>
-          <div className="col-4">
-            <div className="card">
-              <div className="card-head text-center p-2">lista di film</div>
-              <div className="card-body">imput per filtraggio</div>
-            </div>
-          </div>
+          {movies.map((movie, i) => {
+            return (
+              <div className="col-4">
+                <div className="card">
+                  <div className="card-head text-center p-2">
+                    {"Titolo = " + movie.title}
+                  </div>
+                  <hr />
+                  <div className="card-body text-center ">
+                    {"Genere = " + movie.genre}
+                  </div>
+                </div>
+              </div>
+            );
+          })}
         </div>
-        <div className="card">
-          <div className="card-head">lista di film</div>
-          <div className="card-body">imput per filtraggio</div>
-        </div>
+      </div>
+      <div className="card">
+        <label for="disabledSelect" class="form-label p-2">
+          Scegli un genere
+        </label>
+        <select
+          id="disabledSelect"
+          class="form-select"
+          aria-placeholder="Scegli un genere"
+        >
+          {movies.map((generi, i) => {
+            return (
+              <option>
+                <ul>
+                  <li>{generi.genre}</li>
+                </ul>
+              </option>
+            );
+          })}
+        </select>
       </div>
     </>
   );
