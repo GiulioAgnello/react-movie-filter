c15e00013b9c6566687865fc341a780051723c90
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2025-05-27 15:30:19 +0200

    grid

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Main from "./components/Main";
 
+import movies from "./assets/db/Movies.js";
+
 export default function App() {
   return (
     <>
       <Header />
-      <Main />
+      <Main movies={movies} />
       <Footer />
     </>
   );
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,3 +1,32 @@
-export default function Main() {
-  return <h1>Main</h1>;
+export default function Main({ movies }) {
+  return (
+    <>
+      <div className="container">
+        <div className="row g-2 my-2 mt-5">
+          <div className="col-4">
+            <div className="card">
+              <div className="card-head text-center p-2">lista di film</div>
+              <div className="card-body">imput per filtraggio</div>
+            </div>
+          </div>
+          <div className="col-4">
+            <div className="card">
+              <div className="card-head text-center p-2">lista di film</div>
+              <div className="card-body">imput per filtraggio</div>
+            </div>
+          </div>
+          <div className="col-4">
+            <div className="card">
+              <div className="card-head text-center p-2">lista di film</div>
+              <div className="card-body">imput per filtraggio</div>
+            </div>
+          </div>
+        </div>
+        <div className="card">
+          <div className="card-head">lista di film</div>
+          <div className="card-body">imput per filtraggio</div>
+        </div>
+      </div>
+    </>
+  );
 }
